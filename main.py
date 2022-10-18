caf7d0c4e2baf5c9fef65c791a58192c603272ce
Antoine <thoino@outlook.fr>
2022-10-18 23:34:33 +0200

    ajout de commentaires et optimisation

--- main.py
+++ main.py
@@ -14,48 +14,52 @@ def game_loop(g: PresidentGame):
     Args:
         g: The President Game instance.
     """
-    # créer liste de joueur pour simuler tour de table
-    player_table = []
-    player_table.append(g.main_player)
-    for ai_player in g.ai_players:
-        player_table.append(ai_player)
 
-
-    # boucle rejouer une partie
     wanna_continue = True
     while wanna_continue:
-
-        # boucle rejouer les manches
-        # la manche recommence quand tout le monde a passé sauf 1
-        # Respecter ordre + premier joueur
 
         g.round.next_round()
 
         while not g.round.is_ended():
-            if not isinstance(player_table[g.round.current_player], AIPlayer):
+            # vérification du type de joueur : humain ou IA
+            if not isinstance(g.players[g.round.current_player], AIPlayer):
                 print('Your current deck is : ')
                 print(g.main_player.hand)
                 print_ln()
-
+                # si il y à déjà des cartes en jeux
+                # le joueur est contraint de jouer un certain nombre de cartes
+                # et une valeur minimum
                 if g.round.is_started:
                     choice = None
-                    while choice == '' or choice is None or\
-                            not (g.round.cards_on_table[0].is_le(choice) and len(g.round.cards_on_table) <= player_table[g.round.current_player].has_symbol(choice)):
+                    choice_nb_cards = 0
+                    # tant que la valeur jouée n'est pas supérieur à la valeur de la table
+                    # et que la valeur en question n'est pas en nombre supérieur ou égale dans le jeu du joueur
+                    # par rapport au nombre de cartes sur la table, on demande une valeur
+                    while choice == '' or choice is None or \
+                            not (g.round.cards_on_table[0].is_le(choice) and len(g.round.cards_on_table) <= g.players[
+                                g.round.current_player].has_symbol(choice)):
                         choice = input('What value do you wish to play ? pass(p)')
                         if choice == 'p':
                             break
                         choice_nb_cards = len(g.round.cards_on_table)
+                # il n'y a pas de carte en jeu, pas de contrainte de valeur ou de nombre de cartes
                 else:
                     choice = '0'
                     choice_nb_cards = 0
                     while g.main_player.has_symbol(choice) == 0:
                         choice = input('What value do you wish to play ?')
+                    # si le joueur à plusieur fois la même carte, on lui demande le nombre de cartes
+                    # qu'il veut poser
                     if g.main_player.has_symbol(choice) != 1:
+                        # tant que le nombre demander est supérieur au nombres de cartes possédé,
+                        # on refait la demande
                         while choice_nb_cards == '' or g.main_player.has_symbol(
                                 choice) < choice_nb_cards or choice_nb_cards < 1:
                             choice_nb_cards = input(f'How many {choice} do you want to play ?')
                             if choice_nb_cards != '':
                                 choice_nb_cards = int(choice_nb_cards)
+                    # le joueur à la carte en un seul exemplaire, on ne demande pas le nombre de
+                    # cartes qu'il veut poser
                     else:
                         choice_nb_cards = 1
 
@@ -65,29 +69,30 @@ def game_loop(g: PresidentGame):
                 print(f"You play {plays}")
 
                 nb_cards = len(plays)
+            # C'est à L'IA de jouer
             else:
-                #print('symbol : {}  /  nb cartes : {}' .format(g.round.cards_on_table[0].symbol, nb_cards))
+                # print('symbol : {}  /  nb cartes : {}' .format(g.round.cards_on_table[0].symbol, nb_cards))
                 if g.round.is_started:
-                    plays = player_table[g.round.current_player].play(g.round.cards_on_table[0].symbol, len(g.round.cards_on_table))
+                    plays = g.players[g.round.current_player].play(g.round.cards_on_table[0].symbol,
+                                                                   len(g.round.cards_on_table))
                 else:
-                    plays = player_table[g.round.current_player].play('3', 1)
+                    plays = g.players[g.round.current_player].play('3', 1)
                 # si le nombre de carte joué est supèrieur à 0 le dernier joueur ayant joué est le joueur actuel
-                # Update latest card played
                 if len(plays) > 0:
                     g.round.update(g.round.current_player, plays)
-                print(f"{player_table[g.round.current_player].name} plays \t {plays}")
+                print(f"{g.players[g.round.current_player].name} plays \t {plays}")
 
             g.round.set_current_player(g.round.current_player + 1)
-            if g.round.current_player > len(player_table) - 1 :
+            if g.round.current_player > len(g.players) - 1:
                 g.round.set_current_player(0)
 
-        print('Tour remporté par {} ! à lui/elle de commencer.' .format(player_table[g.round.current_player].name))
+        print(
+            '************************************************\nTour remporté par {} ! à lui/elle de '
+            'commencer.\n************************************************\n'.format(
+                g.players[g.round.current_player].name))
 
-
-
-
-        wanna_continue = input('Tour suivant (y/N)? ')
-        wanna_continue = (wanna_continue == 'Y' or wanna_continue == 'y')
+        # wanna_continue = input('Tour suivant (y/N)? ')
+        # wanna_continue = (wanna_continue == 'Y' or wanna_continue == 'y')
 
 
 if __name__ == '__main__':
