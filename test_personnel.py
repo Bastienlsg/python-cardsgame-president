169efee7a69d3b17d4eb871cfb335542300c5319
Bastienlsg <bastien.lassaigne@gmail.com>
2022-10-21 12:14:05 +0200

    Ajout test unitaire

---
+++ test_personnel.py
@@ -0,0 +1,21 @@
+import io
+import sys
+import unittest
+import models
+import re
+
+
+class TestPlayers(unittest.TestCase):
+
+    def test_player_printer(self):
+        g = models.PresidentGame()
+        capturedOutput = io.StringIO()  # Create StringIO object
+        prevStdout = sys.stdout
+        sys.stdout = capturedOutput  # and redirect stdout.
+        g.introduction_player()
+        sys.stdout = prevStdout  # Reset redirect.
+        output_string = str(capturedOutput.getvalue())
+        number = re.findall(r'\d+', output_string)
+        self.assertFalse(len(number) == 0)
+
+
